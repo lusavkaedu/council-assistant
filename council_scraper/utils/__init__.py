a3714e8c2f4f40d92a2266ec8615e0352d3689bf
lusavkaedu <lusavkaedu@gmail.com>
2025-05-28 18:36:19 +0100

    Clean repository - remove development files, keep only production app

--- council_scraper/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# utils/__init__.py
-# This file makes the utils directory a proper Python package
-
-BASE_URL = "https://democracy.kent.gov.uk"
-
-# You can expose the main functions here to simplify imports
-from .http_utils import get_random_headers, create_session, request_with_retry #done
-from .parsing_utils import clean_day_suffix, load_seen_ids, generate_pdf_attachment_check  #done
-from .scraping_utils import scrape_meeting_metadata, run_scrape_batch #done
