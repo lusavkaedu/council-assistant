b63f666b3fd6a0fa7c0fdb682b96745b6a69fac2
lusavkaedu <lusavkaedu@gmail.com>
2025-05-28 19:01:19 +0100

    Clean repository - remove all development files and notebooks

--- scripts/generate_schema_examples.py
+++
@@ -1,4 +0,0 @@
-### Need to create one later: 	
-#  •	Loads first valid entry from each key json file
-#	•	Formats it into markdown
-#	•	Auto-appends or rewrites the ### Example Entry from ... blocks
